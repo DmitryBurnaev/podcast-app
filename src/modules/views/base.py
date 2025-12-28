9d51b9a2e119ebf954eb41f312af2a8571a73368
DmitryBurnaev <dmitry.burnaev@gmail.com>
2025-12-28 14:35:54 +0300

    [#1] Project skeleton: episodes list

--- src/modules/views/base.py
+++ src/modules/views/base.py
@@ -47,6 +47,10 @@ async def episodes() -> Template:
             "current": "episodes",
             "navigation": const.NAVIGATION,
             "user_data": DEFAULT_USER_DATA,
+            "format_duration": const.format_duration,
+            "format_file_size": const.format_file_size,
+            "get_episode_status_color": const.get_episode_status_color,
+            "get_episode_status_label": const.get_episode_status_label,
         },
     )
 
