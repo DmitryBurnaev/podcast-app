eb393946ec9f1c9bf495dc8d464906817316761e
DmitryBurnaev <dmitry.burnaev@gmail.com>
2026-01-02 17:38:01 +0300

    [#3] Make project MVP: app creation + dependencies

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import logging
 import logging.config
 
+import jinja2
 from litestar import Litestar
 from litestar.template import TemplateConfig
 from litestar.static_files import StaticFilesConfig
@@ -11,25 +12,30 @@ from src import settings
 from src.modules.views import *
 
 
-src_root = Path(__file__).parent
-template_config = TemplateConfig(directory=src_root / "templates", engine=JinjaTemplateEngine)
-static_files_config = StaticFilesConfig(path="/static", directories=[str(src_root / "static")])
-
-
 async def startup():
     logging.config.dictConfig(settings.LOGGING)
     logger = logging.getLogger(__name__)
     logger.info("Starting up ...")
 
 
-# Create Litestar application
-app = Litestar(
-    route_handlers=[index, episodes, podcasts, progress, about, profile],
-    template_config=template_config,
-    static_files_config=[static_files_config],
-    on_startup=[startup],
-    debug=True,
-)
+def create_app() -> Litestar:
+    """Simple app creation"""
+    src_root: Path = Path(__file__).parent
+    app = Litestar(
+        route_handlers=[index, episodes, podcasts, progress, about, profile],
+        template_config=TemplateConfig(
+            directory=src_root / "templates", engine=JinjaTemplateEngine
+        ),
+        static_files_config=[
+            StaticFilesConfig(path="/static", directories=[str(src_root / "static")])
+        ],
+        on_startup=[startup],
+        debug=True,
+    )
+    return app
+
+
+app: Litestar = create_app()
 
 
 if __name__ == "__main__":
