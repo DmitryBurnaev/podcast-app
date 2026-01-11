94a2c19072e0f44ab6500cc0f4ff6d29f0e2cfb0
DmitryBurnaev <dmitry.burnaev@gmail.com>
2026-01-11 12:34:42 +0300

    [#3] Make project MVP: podcast details improved

--- src/modules/db/models/podcasts.py
+++ src/modules/db/models/podcasts.py
@@ -21,18 +21,32 @@ from src.utils import utcnow
 class EpisodeStatus(str, Enum):
     """Episode status enumeration"""
 
-    NEW = "new"
-    PENDING = "pending"
-    DOWNLOADING = "downloading"
-    CANCELING = "canceling"
-    PUBLISHED = "published"
-    ERROR = "error"
+    NEW = "NEW"
+    DOWNLOADING = "DOWNLOADING"
+    CANCELING = "CANCELING"
+    PUBLISHED = "PUBLISHED"
+    ARCHIVED = "ARCHIVED"
+    ERROR = "ERROR"
+
+    # sub-statuses for DOWNLOADING (that statuses are not written to the DB, just for progress):
+    DL_PENDING = "DL_PENDING"
+    DL_EPISODE_DOWNLOADING = "DL_EPISODE_DOWNLOADING"
+    DL_EPISODE_POSTPROCESSING = "DL_EPISODE_POSTPROCESSING"
+    DL_EPISODE_UPLOADING = "DL_EPISODE_UPLOADING"
+    DL_COVER_DOWNLOADING = "DL_COVER_DOWNLOADING"
+    DL_COVER_UPLOADING = "DL_COVER_UPLOADING"
+
+    @classmethod
+    def members(cls) -> list[str]:
+        return [status for status in cls.__members__ if not status.startswith("DL_")]
 
 
 class SourceType(str, Enum):
     """Episode source type enumeration"""
 
-    YOUTUBE = "youtube"
+    YOUTUBE = "YOUTUBE"
+    YANDEX = "YANDEX"
+    UPLOAD = "UPLOAD"
 
 
 class Podcast(BaseModel):
@@ -57,7 +71,7 @@ class Podcast(BaseModel):
     # relations
     rss: Mapped["File"] = relationship("File", foreign_keys=[rss_id], lazy="subquery")
     image: Mapped["File"] = relationship("File", foreign_keys=[image_id], lazy="subquery")
-    # episodes: Mapped["Episode"] = relationship("Episode", lazy="subquery", backref="podcast")
+    episodes: Mapped[list["Episode"]] = relationship(back_populates="podcast", lazy="subquery")
 
     def __str__(self):
         return f'<Podcast #{self.id} "{self.name}">'
@@ -180,7 +194,7 @@ class Episode(BaseModel):
     published_at: Mapped[datetime | None] = mapped_column(sa.DateTime(timezone=True), nullable=True)
 
     # relations
-    podcast: Mapped["Podcast"] = relationship("Podcast", lazy="subquery", backref="episodes")
+    podcast: Mapped["Podcast"] = relationship(back_populates="episodes", lazy="subquery")
     image: Mapped["File"] = relationship("File", foreign_keys=[image_id], lazy="subquery")
     audio: Mapped["File"] = relationship("File", foreign_keys=[audio_id], lazy="subquery")
 
