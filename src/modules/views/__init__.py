370c6a8d561068f4e9fff2fc13abc87b70d3c71a
DmitryBurnaev <dmitry.burnaev@gmail.com>
2026-02-21 12:00:10 +0300

    [#3] Make project MVP: some fixes for episodes' creation

--- src/modules/views/__init__.py
+++ src/modules/views/__init__.py
@@ -1,12 +1,17 @@
 from .index import IndexController
-from .podcasts import EpisodesController, PodcastsController
+from .podcasts import PodcastsController, PodcastCoverController, PodcastsDetailsController
+from .episodes import EpisodesController, EpisodeCoverController, EpisodeDetailsController
 from .system import AboutController
 from .users import ProfileController
 
 __all__ = (
     "IndexController",
     "EpisodesController",
+    "EpisodeCoverController",
+    "EpisodeDetailsController",
     "PodcastsController",
+    "PodcastsDetailsController",
+    "PodcastCoverController",
     "ProfileController",
     "AboutController",
 )
